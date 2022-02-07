340095086abbfaa59bdfe180f653f254784d411d
omarelmellouki <omar.elmellouki.9797@gmail.com>
2022-02-07 17:36:48 +0100

    env setting

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import numpy
+"""Setting up the Mario Environment"""
+# Import the game
+import gym_super_mario_bros
+# Import the Joypad wrapper
+from nes_py.wrappers import JoypadSpace
+# Import the SIMPLIFIED controls
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+# Important to simplify the environement 
+print(SIMPLE_MOVEMENT)
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+# To get
+print(env.observation_space.shape)
